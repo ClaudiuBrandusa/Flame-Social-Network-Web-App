7884661f43eb1eee450cbe3f2f827318cd5e80ed
ClaudiuBrandusa <claudiu.office99@gmail.com>
2020-09-19 17:01:35 +0300

    Run JS Client Chat on each page where you are signed in

---
+++ Flame-Social-Network-Web-App/wwwroot/js/chatClient.js
@@ -0,0 +1,43 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub", {
+        accessTokenFactory: () => "testing"
+    })
+    .build();
+// Connection events
+connection.on("UserConnected", function () {
+    connection.invoke("ListContacts");
+});
+
+// Contacts List
+connection.on("ListContacts", function (list) {
+    // we are looking for the contact list
+    var ul = document.getElementById("list");
+    if (ul == null) {
+        return;
+    }
+    if (list.length == 0) {
+        ul.innerHTML = "There are no online contacts in the list";
+        return;
+    }
+    // clearing the list
+    ul.innerHTML = "";
+    // configuring the list's rows
+    for (var i = 0; i < list.length; i++) {
+        var row = document.createElement("li");
+        row.classList.add("no-list-style");
+        var anchor = document.createElement("a");
+        anchor.addEventListener("click", function () {
+            connection.invoke("ChatWith", anchor.innerText);
+        });
+        anchor.classList.add("link");
+        row.appendChild(anchor);
+        anchor.innerHTML = list[i];
+        ul.appendChild(row);
+    }
+});
+
+connection.on("alert", function (text) {
+    alert(text);
+});
